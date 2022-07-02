e8bf566ac4e1af862470d4957727e4ba8260eef1
piratelicorne <thecodeloops@gmail.com>
2022-07-02 03:04:53 -0500

    amoors-item design: heart on the bottom corner, changes color when its celebration's date matches to today's date

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,21 @@
 //Capitalize string's first letter
 function titleCase(str) {
-    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
 
 //Check if a string contains only letters
 function onlyLetters(str) {
-    return /^[a-zA-Z]+$/.test(str);
+  return /^[a-zA-Z]+$/.test(str);
 }
 
-module.exports = {titleCase, onlyLetters}
+//Check if a string contains only letters
+function isCelebrationDay(date) {
+  const today = new Date();
+
+  return (
+    parseInt(date.substring(8, 10)) == today.getDate() &&
+    parseInt(date.substring(5, 7)) == today.getMonth() + 1
+  );
+}
+
+module.exports = { titleCase, onlyLetters, isCelebrationDay };
